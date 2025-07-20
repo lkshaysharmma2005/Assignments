174ae844ce75bde7bd1341f101262d53515ae5c3
LakshaySharma1108 <sharmmalkshay@gmail.com>
2025-07-20 23:11:56 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function setDarkorLight() {
+  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
+  } else {
+    document.body.classList.add("light");
+    document.body.classList.remove("dark");
+  }
+}
+setDarkorLight();
+
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", function(){
+  setDarkorLight();
+});
+
+
+let btn = document.querySelector("#toggleTheme")
+
+btn.addEventListener("click", function(){
+  document.body.classList.toggle("dark");
+});
